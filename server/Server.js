2169981b1d779ca3245da4e03573bfa0b52dcd1d
Jaswanth Chowdary <kittunaidu888@gmail.com>
2025-09-03 21:06:32 +0530

    Prepare full-stack app with public folder React

--- server/Server.js
+++ server/Server.js
@@ -197,7 +197,7 @@ app.post("/api/profile", async (req, res) => {
 // Serve React build static files
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
